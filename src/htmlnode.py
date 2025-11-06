924fbb56d1ec522a58a7072b0d331d121f0d4d5e
Tyler Pearson <tyler.a.pear@gmail.com>
2025-11-05 19:23:33 -0800

    adds leafnode with unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,15 +21,18 @@ class HTMLNode():
     
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
-            
-
-def test():
-    props = {
-    "href": "https://www.google.com",
-    "target": "_blank",
-    }
+        
+        
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+        
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return f"{self.value}"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
     
-    node = HTMLNode(props=props)
-    print(node.props_to_html())
-    
-test()
+    def __repr__(self):
+        return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html(self):
@@ -20,11 +20,31 @@ class TestHTMLNode(unittest.TestCase):
         childNode = HTMLNode()
         props = {"color": "red"}
         node = HTMLNode(tag="h1", value="testval", children=[childNode], props=props)
-        print(node)
         self.assertEqual(
             repr(node), 
             "HTMLNode(tag=h1, value=testval, children=[HTMLNode(tag=None, value=None, children=None, props=None)], props={'color': 'red'})"
         )
+        
+    ### Test LeafNode
+        
+    def test_leaf_init(self):
+        node = LeafNode("p", "Hello World!", {"color": "red"})
+        self.assertEqual(
+            repr(node),
+            "LeafNode(tag=p, value=Hello World!, props={'color': 'red'})"
+        )
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello World!")
+        self.assertEqual(node.to_html(), "<p>Hello World!</p>")
+        
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello World!")
+        self.assertEqual(node.to_html(), "Hello World!")
+        
+    def test_leaf_to_html_props(self):
+        node = LeafNode("p", "Hello World!", {"color": "red", "font": "arial"})
+        self.assertEqual(node.props_to_html(), ' color="red" font="arial"')
 
 
 if __name__ == "__main__":
